670c8c21b4b4a511e0466c7a21586900503c0245
Nastia Tarasenko <anastasiatarasenko679@gmail.com>
2025-08-11 21:13:10 +0200

    Update Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./app/App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/fcc-react-portfolio">
+            {/* <BrowserRouter basename="/fcc-react-portfolio"> */}
+            <HashRouter>
+                {/* Using HashRouter for GitHub Pages compatibility */}
                 <App />
-            </BrowserRouter>
+            </HashRouter>
+            {/* </BrowserRouter> */}
         </Provider>
     </StrictMode>
 );
