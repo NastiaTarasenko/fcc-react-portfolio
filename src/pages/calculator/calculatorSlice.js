036a046296e49988e8abb28dbdde4c0432487d3e
Nastia Tarasenko <anastasiatarasenko679@gmail.com>
2025-08-13 20:25:46 +0200

    refactor: move operator logic to addOperator reducer

--- src/pages/calculator/calculatorSlice.js
+++ src/pages/calculator/calculatorSlice.js
@@ -1,4 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { OPERATIONS } from "./constants";
+
+const addOperatorHelper = (state, value) => {
+    const dotMultiplication = value === "x" && "·";
+
+    if (state.wasEqualed) {
+        const result = state.input.split("=")[1] || "";
+        state.input = result + (dotMultiplication || value);
+        state.lastValue = value;
+        state.wasEqualed = false;
+    } else if (state.input === "" || state.input === "-") {
+        if (value === "-") {
+            state.input = "-";
+            state.lastValue = "-";
+        } else {
+            state.input = "";
+            state.lastValue = value;
+        }
+    } else if (OPERATIONS.includes(state.lastValue)) {
+        if ((state.lastValue === "x") & (value == "-")) {
+            state.input += dotMultiplication || value;
+            state.lastValue = value;
+        } else if (state.lastValue === "-") {
+            let expr = state.input;
+
+            while (/[+\-*/.·]$/.test(expr)) {
+                expr = expr.slice(0, -1);
+            }
+
+            state.input = expr + (dotMultiplication || value);
+            state.lastValue = value;
+        } else {
+            state.lastValue = value;
+            if (value !== "-" && state.input == "-") {
+                state.input = "0";
+            } else {
+                state.input = state.input.slice(0, -1) + (dotMultiplication || value);
+            }
+        }
+    } else {
+        state.lastValue = value;
+        state.input += dotMultiplication || value;
+    }
+};
 
 const CalculatorSlice = createSlice({
     name: "calculator",
@@ -28,10 +72,12 @@ const CalculatorSlice = createSlice({
             if (digitLimitMet !== undefined) state.digitLimitMet = digitLimitMet;
             if (wasEqualed !== undefined) state.wasEqualed = wasEqualed;
         },
+        addOperator: (state, action) => addOperatorHelper(state, action.payload),
     },
 });
 
-const { setInput, setLastValue, setDigitLimitMet, setWasEqualed, setCalculatorState } = CalculatorSlice.actions;
+const { setInput, setLastValue, setDigitLimitMet, setWasEqualed, setCalculatorState, addOperator } =
+    CalculatorSlice.actions;
 
-export { setInput, setLastValue, setDigitLimitMet, setWasEqualed, setCalculatorState };
+export { setInput, setLastValue, setDigitLimitMet, setWasEqualed, setCalculatorState, addOperator };
 export default CalculatorSlice.reducer;
